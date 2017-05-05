7ca4eb9e7cd74489fa17c67ec5bacffc40cc7ce2
dylanhart <dylan96hart@gmail.com>
2017-05-04 20:56:51 -0700

    Initial Commit

---
+++ AutoBeacon/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AutoBeacon
+//
+//  Created by Dylan Hart on 5/4/17.
+//  Copyright © 2017 Dylan Hart. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ AutoBeacon/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AutoBeacon
+//
+//  Created by Dylan Hart on 5/4/17.
+//  Copyright © 2017 Dylan Hart. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
